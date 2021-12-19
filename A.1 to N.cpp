08d7ecd64ea239e560692b2ce3a4e39caf2978a3
TheHonestGuy <92696767+Arif-Rahman-Sikder@users.noreply.github.com>
2021-12-19 21:57:22 +0600

    Create A.1 to N.cpp
    First basic loop problem to print from 1 to N serially.
    So we used the for loop for that.
    (++) means increment.We can say that i++ which means i=i+1.

---
+++ A.1 to N.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int N, i;
+  cin >> N;
+  for (i = 1; i <= N; i++) //for loop, (++) means increment 
+    {
+      cout << i << endl;
+    }
+  return 0;
+}
