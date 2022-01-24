3403651caf125ff1726d18565198c6100b040f99
TheHonestGuy <92696767+Arif-Rahman-Sikder@users.noreply.github.com>
2022-01-24 11:05:09 +0600

    Create D - Fixed Password.cpp

---
+++ D - Fixed Password.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int value=0;
+ 
+        while(cin>>value){
+        
+        if(value==1999){
+            cout<<"Correct"<<endl;
+            break;
+        }else cout<<"Wrong"<<endl;
+    }
+     return 0;
+}
