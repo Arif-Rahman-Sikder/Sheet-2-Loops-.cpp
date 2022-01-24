2740c05c07bda324a7838d1c55866de5e726bdb0
TheHonestGuy <92696767+Arif-Rahman-Sikder@users.noreply.github.com>
2022-01-24 12:30:12 +0600

    Update B. Even Numbers.cpp

--- B. Even Numbers.cpp
+++ B. Even Numbers.cpp
@@ -1,19 +1,17 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int N,i;
-
+    int N;
     cin>>N;
-    if(N==0||N==1||N==-1){ //OR operator
-        cout<<-1<<endl;
-    }
-    
-    else if(N>1){
+    for(int i=2;i<=N;i++){
         
-        for(i=2;i<=N;i+=2){
-            cout<<i<<endl;
-        }
+        if(i%2 !=0){
+            continue;
+        }else cout<<i<<endl;
     }
-return 0;
+    if(N==1 ||N==-1||N==0){
+        cout<<"-1"<<endl;
+    }
+     return 0;  
 }
 
