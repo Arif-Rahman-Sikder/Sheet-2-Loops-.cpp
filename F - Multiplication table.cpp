c5f89ad141de95346d2d876133382f5748e6d878
TheHonestGuy <92696767+Arif-Rahman-Sikder@users.noreply.github.com>
2022-01-24 12:23:09 +0600

    Create F - Multiplication table.cpp

---
+++ F - Multiplication table.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int N, x;
+  cin >> N;
+  for (x = 1; x<=12 ; x++)
+    {
+      cout << N << " * " << x << " = " << N * x << endl;
+    }
+ return 0;
+}
